0901ec76e2adbd9311e7d3f665bd96c9a76d8b67
Qi Shao <qi.shao.nyu@gmail.com>
2015-04-06 01:10:03 -0400

    Create tokenremover.py

---
+++ tokenremover.py
@@ -0,0 +1,25 @@
+import sys
+import re
+
+_digits = re.compile('\d')
+_nonenglish = re.compile('[\x80-\xFF]')
+_tags = re.compile("[`|'|$|^|%|&|@|#|]")
+def containDigit(token):
+    return bool(_digits.search(token))
+
+def containNonEnglish(token):
+	return bool(_nonenglish.search(token))
+
+def containtags(token):
+	return
+
+tags = ("`","'","#","\"","$","%","^","&","*","@","-","~","/","\\","_","+","=","-","|","<",">")
+
+for line in sys.stdin:
+	words = line.split(" ")
+	words = [word for word in words if not containDigit(word) 
+	and not (word in tags) 
+	and not containNonEnglish(word)
+	and word.strip(''.join(tags))]
+
+	sys.stdout.write(" ".join(words))
